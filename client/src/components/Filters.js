a97d2e8bca059e481b1423882ba401dbfdf4fd9a
FacuMeoni <meonifacu@gmail.com>
2024-11-07 12:01:33 -0300

    feat: add custom hook for query filters

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -1,61 +1,25 @@
-import { useNavigate, useLocation } from "react-router-dom"
-import { useState, useEffect } from "react";
+import useQueryFilters from "../hooks/useQueryFilters";
 
 
 const Filters = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const queryParams = new URLSearchParams(location.search);
-    const [filters, setFilters ] = useState({
-        status: queryParams.get("status")|| "" ,
-        gender: queryParams.get("gender")|| "",
-        page: Number(queryParams.get("page") || 1)
-    });
-
-    function handleUpdateQuery (key, value) {
-        if(queryParams.get(key) === value) {
-            queryParams.delete(key);
-            setFilters({
-                ...filters,            
-                [key]: ""
-            });
-        } else { 
-            queryParams.set(key, value);
-            queryParams.set("page", 1);
-            setFilters({
-                ...filters,
-                [key]: value
-            });
-        }
-
-       navigate({ search: queryParams.toString(location.search)});
-    }
-
-    function resetFilters() {
-        queryParams.delete("gender"); 
-        queryParams.delete("status"); 
-        queryParams.set("page", 1);
-
-        setFilters({ status: "", gender: "", page: 1 });
-        navigate({ search: queryParams.toString() });
-    }
-
+    const { updateQuery, resetQuery } = useQueryFilters();
+    
     return(
         <div>
             <div>
-                <button onClick={() => handleUpdateQuery("status", "Alive")}>Alive</button>
-                <button onClick={() => handleUpdateQuery("status", "Dead")} >Dead</button>
-                <button onClick={() => handleUpdateQuery("status", "Unknown")}>Unknown</button>
+                <button onClick={() => updateQuery("status", "Alive")}>Alive</button>
+                <button onClick={() => updateQuery("status", "Dead")} >Dead</button>
+                <button onClick={() => updateQuery("status", "Unknown")}>Unknown</button>
             </div>
             
 
             <div>
-                <button onClick={() => handleUpdateQuery("gender", "Male")}>Male</button>
-                <button onClick={() => handleUpdateQuery("gender", "Female")}>Female</button>
-                <button onClick={() => handleUpdateQuery("gender", "Genderless")}>Genderless</button>
+                <button onClick={() => updateQuery("gender", "Male")}>Male</button>
+                <button onClick={() => updateQuery("gender", "Female")}>Female</button>
+                <button onClick={() => updateQuery("gender", "Genderless")}>Genderless</button>
             </div>
 
-            <button onClick={resetFilters}>All</button>
+            <button onClick={resetQuery}>All</button>
         </div>
     )
 }
---
+++ client/src/hooks/useQueryFilters.js
@@ -0,0 +1,47 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from 'react';
+
+const useQueryFilters = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const queryParams = new URLSearchParams(location.search);
+
+    const [ filters, setFilters ] =useState({
+        status: queryParams.get("status")|| "" ,
+        gender: queryParams.get("gender")|| "",
+        currentPage: Number(queryParams.get("page") || 1)
+    });
+
+    const updateQuery = (key, value) => {
+        if(queryParams.get(key) === value) {
+            queryParams.delete(key);
+            setFilters({
+                ...filters,            
+                [key]: ""
+            });
+        } else { 
+            queryParams.set(key, value);
+            queryParams.set("page", 1);
+            setFilters({
+                ...filters,
+                [key]: value
+            });
+        }
+
+       navigate({ search: queryParams.toString(location.search)});
+    }
+
+    const resetQuery = () => {
+        queryParams.delete("gender"); 
+        queryParams.delete("status"); 
+        queryParams.set("page", 1);
+
+        setFilters({ status: "", gender: "", currentPage: 1 });
+        navigate({ search: queryParams.toString() });
+    }
+    
+    return { filters, updateQuery, resetQuery }
+}
+
+
+export default useQueryFilters;
