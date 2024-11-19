e55a0f2b80af44fa92024ba8b6fbafb47c73b1ad
FacuMeoni <meonifacu@gmail.com>
2024-11-19 13:06:19 -0300

    refactor: fix problem with OpiLike on filters

--- server/src/utils/filter_characters.js
+++ server/src/utils/filter_characters.js
@@ -4,9 +4,9 @@ import { Op } from "sequelize";
 export const filterCharacters = ({ status, gender, species, origin, name }) => {
     let where = {};
     
-    if(status) where.status = { [Op.iLike] : `%${status}%`}
+    if(status) where.status = status
     if(name) where.name = { [Op.iLike] : `%${name}%`}
-    if(gender) where.gender = { [Op.iLike] : `%${gender}%`}
+    if(gender) where.gender = gender
     if(species) where.species = { [Op.iLike] : `%${species}%`}
     if(origin) where.origin = { [Op.iLike] : `%${origin}%`}
 
