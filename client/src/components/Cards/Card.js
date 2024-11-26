46b7a02dc5ab05362645af3331800fa860f6fa37
FacuMeoni <meonifacu@gmail.com>
2024-11-25 23:29:42 -0300

    feat: add

--- client/src/components/Cards/Card.js
+++ client/src/components/Cards/Card.js
@@ -1,13 +1,27 @@
 import './Cards.css';
 import { GenderSvg } from '../../assets/GenderSvg';
 import SetStatusByBackground from '../../utils/SetBackgroundByStatus';
+import LoadingImage  from '../../assets/LoadingImage.webp';
+import { useState } from 'react';
 
 function Card ({ id, name, image, status, gender, species, onClick }) {
+
+    const [ImageLoaded, setImageLoaded] = useState(false);
+
+    const setLoadingImage = () => setImageLoaded(true);
     
     return (
         <article id="card">  
             <header className="card-header">      
-                <img src={image} alt={`${name}`} className='card-image'/>
+                {image && !ImageLoaded && <img
+                        src={LoadingImage}
+                        alt='loading-image'
+                        className="card-image loading"
+                    />} 
+                {image && (
+                   <img src={image} alt={`${name}`} className={`card-image ${ImageLoaded ? '' : 'hidden'}`}
+                   onLoad={setLoadingImage}/> 
+                )}
             </header>
             <main className="card-main" style={{ backgroundColor: SetStatusByBackground(status)}}>
                 <h2 className="card-title" onClick={onClick}> { name } </h2>
